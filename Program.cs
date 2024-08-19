9304e770e456e5cb4ccbb1a78504e54c0319c76d
Hall1110 <160109460+Hall1110@users.noreply.github.com>
2024-08-19 14:32:23 +0200

    Yahtzee
    I made a mess of things

--- Program.cs
+++ Program.cs
@@ -72,6 +72,7 @@ tag summen af lower card
             {
                 playerHand[i] = dice.Roll();
             }
+            
         }
 
 
@@ -88,29 +89,239 @@ tag summen af lower card
 
 
 
-        public static void CheckMatching(int[] playerHand)
+        public static int CheckMatchingAces(int[] playerHand)
         {
             int playerHandLength = playerHand.Length;
-            int matchingNumbersCounter = 0;
+            int acesAmount = 0;
+            int acesSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 1)
+                {
+                    acesAmount++;
+                }
+            }
+            for (int i = 0; i < acesAmount; i++)
+            {
+                acesSum =+ 1;
+            }
+
+            return acesSum;
+        }
+
+
+        public static int CheckMatchingTwos(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int TwosAmount = 0;
+            int TwosSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 2)
+                {
+                    TwosAmount++;
+                }
+            }
+            for (int i = 0; i < TwosAmount; i++)
+            {
+                TwosSum = +1;
+            }
+
+            return TwosSum;
+        }
+
+
+
+        public static int CheckMatchingThrees(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int threesAmount = 0;
+            int threesSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 3)
+                {
+                    threesAmount++;
+                }
+            }
+            for (int i = 0; i < threesAmount; i++)
+            {
+                threesSum = +1;
+            }
+
+            return threesSum;
+        }
+
+
+
+        public static int CheckMatchingFours(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int foursAmount = 0;
+            int foursSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 4)
+                {
+                    foursAmount++;
+                }
+            }
+            for (int i = 0; i < foursAmount; i++)
+            {
+                foursSum = +1;
+            }
+
+            return foursSum;
+        }
+
+
+
+        public static int CheckMatchingFives(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int fivesAmount = 0;
+            int fivesSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 5)
+                {
+                    fivesAmount++;
+                }
+            }
+            for (int i = 0; i < fivesAmount; i++)
+            {
+                fivesSum = +1;
+            }
+
+            return fivesSum;
+        }
+
+
+
+
+        public static int CheckMatchingSixes(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int sixesAmount = 0;
+            int sixesSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                if (playerHand[i] == 6)
+                {
+                    sixesAmount++;
+                }
+            }
+            for (int i = 0; i < sixesAmount; i++)
+            {
+                sixesSum = +1;
+            }
+
+            return sixesSum;
+        }
+
+
+
+        public static int UseChance(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int chanceSum = 0;
+
+            for (int i = 0; i < playerHandLength; i++)
+            {
+                chanceSum += playerHand[i];
+            }
+            return chanceSum;
+        }
+        
+
+
+        public static int ThreeOfAKind(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int threeOfAKindSum = 0;
+
+            for (int i = 1; i < playerHandLength - 1; i++)
+            {
+                int firstValue = playerHand[i - 1];
+                int secondValue = playerHand[i];
+                int thirdValue = playerHand[i + 1];
+
+                if (firstValue == secondValue && secondValue == thirdValue)
+                {
+                    threeOfAKindSum += firstValue + secondValue + thirdValue;
+                }
+            }
+            return threeOfAKindSum;
+        }
+
+
+
+        public static int FourOfAKind(int[] playerHand)
+        {
+            int playerHandLength = playerHand.Length;
+            int fourOfAKindSum = 0;
+
+            for (int i = 2; i < playerHandLength - 1; i++)
+            {
+                int firstValue = playerHand[i - 2];
+                int secondValue = playerHand[i - 1];
+                int thirdValue = playerHand[i];
+                int fourthValue = playerHand[i + 1];
+
+                if (firstValue == secondValue && secondValue == thirdValue && thirdValue == fourthValue)
+                {
+                    fourOfAKindSum += firstValue + secondValue + thirdValue + fourthValue;
+                }
+            }
+            return fourOfAKindSum;
+        }
+
+
+
+        public static int FullHouse(int[] playerHand)
+        {
+            bool twoMatching = false;
+            bool threeMatching = false;
+            int playerHandLength = playerHand.Length;
+            int firstIndexedValue = playerHand[0];
+            int lastIndexedValue = playerHand[4];
             
 
-            for (int i = 1; i < playerHandLength; i++)
+            for (int i = 2; i < playerHandLength; i++)
             {
-                if (playerHand[i] == playerHand[i - 1])
+                if ((firstIndexedValue == playerHand[i - 1] && firstIndexedValue == playerHand[i]) 
+                    || 
+                   lastIndexedValue == playerHand[i - 1] && lastIndexedValue == playerHand[i - 2])
                 {
-                    matchingNumbersCounter++;
+                    threeMatching = true;
+                }
+                else if ((firstIndexedValue == playerHand[i - 1]) || lastIndexedValue == playerHand[i - 1]) // skuffed
+                {
+                    twoMatching = true;
                 }
                 else
                 {
-                    continue;
+                    return 0;
                 }
             }
-            
-            for (int i = 0; i < matchingNumbersCounter; i++)
-            {
 
+            if (twoMatching && threeMatching)
+            {
+            return 25;
+            }
+            else
+            {
+                return 0;
             }
         }
+
+
     }
 }
 
